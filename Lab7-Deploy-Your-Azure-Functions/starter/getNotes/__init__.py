f8ca12ced7c67a3893f8f3ac58829d8366cbfe2b
Luc Cao <luccv.qngnt@gmail.com>
2023-06-21 16:50:20 +0700

    Update __init__.py

--- Lab7-Deploy-Your-Azure-Functions/starter/getNotes/__init__.py
+++ Lab7-Deploy-Your-Azure-Functions/starter/getNotes/__init__.py
@@ -5,19 +5,17 @@ import json
 from bson.json_util import dumps
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-
+    logging.info('Python HTTP trigger function processed a request.')
     try:
-        url = ""
+        id = req.params.get('id')
+        url = os.environ["myAzureCosmosMongoDBConnectionString"]
         client = pymongo.MongoClient(url)
-        database = ""
-        collection = ""
-
-
-        result = collection.find({})
+        database = client['lab2db']
+        collection = database['notes']
+        query = {'_id': ObjectId(id)}
+        result = collection.find_one(query)
         result = dumps(result)
-
-        return func.HttpResponse(result, mimetype="application/json", charset='utf-8')
+        
+        return func.HttpResponse(result, mimetype="application/json", charset="utf-8", status_code=200)
     except:
-        return func.HttpResponse("Could not connect to mongodb",
-                                 status_code=400)
-
+            return func.HttpResponse("Bad Request", status_code=400)
